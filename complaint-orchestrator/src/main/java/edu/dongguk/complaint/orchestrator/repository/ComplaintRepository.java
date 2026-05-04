c8d7185247e183d1edc516be858d11b799234003
sssuhyeon <ckc020506@gmail.com>
2026-05-04 17:37:07 +0900

    feat: 부서isChecked==false인 민원이 있는 부서 id를 ì°반환하는 repository 추가

--- complaint-orchestrator/src/main/java/edu/dongguk/complaint/orchestrator/repository/ComplaintRepository.java
+++ complaint-orchestrator/src/main/java/edu/dongguk/complaint/orchestrator/repository/ComplaintRepository.java
@@ -1,5 +1,6 @@
 package edu.dongguk.complaint.orchestrator.repository;
 
+import edu.dongguk.complaint.orchestrator.domain.Depart;
 import edu.dongguk.complaint.orchestrator.domain.complaint.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,11 +14,15 @@ public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
     @Query("SELECT COUNT(DISTINCT c.depart.id) FROM Complaint c WHERE c.file.id = :fileId AND c.isChecked = true")
     long countCheckedDepartsByFileId(@Param("fileId") Long fileId);
 
-    // 부서별로 분류된 민원 개수 반환
+    // 부서별로 분류된 전체 민원 개수 반환
     @Query("SELECT c.depart, COUNT(c) FROM Complaint c WHERE c.file.id = :fileId AND c.depart IS NOT NULL GROUP BY c.depart")
     List<Object[]> countComplaintsByDepart(@Param("fileId") Long fileId);
 
-    // 특정 부서로 분류되지 않은 민원 개수 반환
+    // 부서별로 분류된 민원 중 isChecked==false인 민원 반환
+    @Query("SELECT c.depart FROM Complaint c WHERE c.file.id = :fileId AND c.depart IS NOT NULL AND c.isChecked = false GROUP BY c.depart")
+    List<Depart> findDepartsWithUnchecked(@Param("fileId") Long fileId);
+
+    // 특정 부서로 분류되지 않은 전체 민원 개수 반환
     @Query("SELECT COUNT(c) FROM Complaint c WHERE c.file.id = :fileId AND c.depart IS NULL")
     Long countUnclassifiedComplaints(@Param("fileId") Long fileId);
 }
