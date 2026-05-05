4b95b6d2d2444c0443fe209a8f2be7cb50866497
sssuhyeon <ckc020506@gmail.com>
2026-05-05 15:47:16 +0900

    feat: create ComplaintRepository

--- complaint-orchestrator/src/main/java/edu/dongguk/complaint/orchestrator/repository/ComplaintRepository.java
+++ complaint-orchestrator/src/main/java/edu/dongguk/complaint/orchestrator/repository/ComplaintRepository.java
@@ -2,6 +2,7 @@ package edu.dongguk.complaint.orchestrator.repository;
 
 import edu.dongguk.complaint.orchestrator.domain.complaint.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,9 @@ public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
 
     @Query("SELECT COUNT(DISTINCT c.depart.id) FROM Complaint c WHERE c.file.id = :fileId AND c.isChecked = true")
     long countCheckedDepartsByFileId(@Param("fileId") Long fileId);
+
+    // 사용자가 선택한 부서에 배부된 민원들의 isChecked를 true로 update하는 query
+    @Modifying
+    @Query("UPDATE Complaint c SET c.isChecked = true WHERE c.file.id = :fileId AND c.depart.id IN :departIds")
+    void checkComplaintsByFileIdAndDepartIds(@Param("fileId") Long fileId, @Param("departIds") List<Long> departIds);
 }
