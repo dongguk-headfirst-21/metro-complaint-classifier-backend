227fa4ec0daf95093471af894ed7ffb8959f21ea
sssuhyeon <ckc020506@gmail.com>
2026-05-05 15:48:53 +0900

    feat: add checkDeparts method

--- complaint-orchestrator/src/main/java/edu/dongguk/complaint/orchestrator/controller/FileController.java
+++ complaint-orchestrator/src/main/java/edu/dongguk/complaint/orchestrator/controller/FileController.java
@@ -1,10 +1,13 @@
 package edu.dongguk.complaint.orchestrator.controller;
 
+import edu.dongguk.complaint.orchestrator.dto.request.DepartListRequestDto;
 import edu.dongguk.complaint.orchestrator.dto.response.FileListResponseDto;
+import edu.dongguk.complaint.orchestrator.service.command.DepartCheckService;
 import edu.dongguk.complaint.orchestrator.service.command.FileUploadService;
 import edu.dongguk.complaint.orchestrator.service.query.FileQueryService;
 import edu.dongguk.complaint.orchestrator.service.sse.SseEmitterService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +23,7 @@ public class FileController {
     private final FileQueryService fileQueryService;
     private final FileUploadService fileUploadService;
     private final SseEmitterService sseEmitterService;
+    private final DepartCheckService departCheckService;
 
 
     @GetMapping
@@ -35,5 +39,14 @@ public class FileController {
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe() {
         return sseEmitterService.subscribe();
+    }
+
+    @PatchMapping(value = "/{fileId}/departs/check")
+    public ResponseEntity<Void> checkDeparts(
+            @PathVariable Long fileId,
+            @RequestBody DepartListRequestDto requestDto
+    ) {
+        departCheckService.checkDeparts(fileId, requestDto);
+        return ResponseEntity.ok().build();
     }
 }
